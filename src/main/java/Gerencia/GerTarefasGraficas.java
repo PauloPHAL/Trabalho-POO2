64fda383d315beaf491a3b89e52dc77c3789bd09
2019122760222 <2019122760222@col-8123-li07.cefetes.br>
2023-03-22 16:04:21 -0300

    Tudo pronto!!!

--- src/main/java/Gerencia/GerTarefasGraficas.java
+++ src/main/java/Gerencia/GerTarefasGraficas.java
@@ -1,21 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Gerencia;
 
-/**
- *
- * @author 2019122760222
- */
-public class GerTarefasGraficas {
+import Interface.Principal;
+import java.awt.Frame;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+public class GerTarefasGraficas {
+    //acessos
+    private Principal frmPrincipal;
+    
+    private GerTarefasDao gerenciaDAO;
+    
+    //construtor
+    public GerTarefasGraficas(){
+        this.frmPrincipal = null;
+//        try {
+//            this.gerenciaDAO = new GerTarefasDao();
+//        } catch (ClassNotFoundException | SQLException ex) {
+//            JOptionPane.showMessageDialog(this.frmPrincipal, ex);
+//            System.exit(-1);
+//        }
+    }
+       
+    //Abrir Janelas   
+    private JDialog abrirJanela(java.awt.Frame parent, JDialog dlg, Class classe){
+        if(dlg == null){
+            try {
+                try {
+                    dlg = (JDialog) classe.getConstructor(Frame.class, boolean.class, GerTarefasGraficas.class).newInstance(parent,true,this);
+                } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                    
+                }
+            } catch (NoSuchMethodException | SecurityException ex) {
+                
+            }       
+        }
+        dlg.setVisible(true);
+        return dlg;
+    }
+    private void abrirPrincipal(){
+        if(this.frmPrincipal == null){
+            frmPrincipal = new Principal(this);
+        }
+        frmPrincipal.setVisible(true);
     }
     
+    //-----------------------------------------------------------------------------------------
+    
+    //main
+    public static void main(String[] args) {
+        try{
+            for(javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()){
+                if("Nimbus".equals(info.getName())){
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            GerTarefasGraficas gerencia = new GerTarefasGraficas();
+            gerencia.abrirPrincipal();
+        }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex){
+            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }  
 }
