f37ad9eabbbcdccea011ffa63aacfe3435d17983
2019122760222 <2019122760222@col-8123-li07.cefetes.br>
2023-03-22 15:17:50 -0300

    Tudo pronto!!!

---
+++ src/main/java/Gerencia/GerTarefasGraficas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gerencia;
+
+/**
+ *
+ * @author 2019122760222
+ */
+public class GerTarefasGraficas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
