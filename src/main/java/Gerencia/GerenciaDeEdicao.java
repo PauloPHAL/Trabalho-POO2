c8f95cb5bfeeafdf7029c08ada38901b9299b990
2019122760222 <2019122760222@col-8123-li07.cefetes.br>
2023-04-28 14:09:07 -0300

    quase pronto :(

---
+++ src/main/java/Gerencia/GerenciaDeEdicao.java
@@ -0,0 +1,14 @@
+package Gerencia;
+
+import Dominio.*;
+
+public class GerenciaDeEdicao {
+    private Aeronave aeronaveSelecionada;
+    private Modelo modeloSelecionado;
+    private Fabricante fabricanteSelecionado;
+    private Cliente clienteSelecionado;
+    private Locacao locacaoSelecionada;
+    
+    
+    
+}
