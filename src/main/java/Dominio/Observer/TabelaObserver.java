7e01a5570c35c8b8ed0a9a79bf2805937535a9c5
2019122760222 <2019122760222@col-8123-li07.cefetes.br>
2023-05-24 16:14:14 -0300

    Padrao Observer Aeronave

--- src/main/java/Dominio/Observer/TabelaObserver.java
+++ src/main/java/Dominio/Observer/TabelaObserver.java
@@ -1,5 +1,9 @@
 package Dominio.Observer;
 
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TabelaObserver extends AeronavesObservers{
 
     public TabelaObserver(DadosSubject dados) {
@@ -8,8 +12,21 @@ public class TabelaObserver extends AeronavesObservers{
 
     @Override
     public void update() {
-        System.out.println(dados.dadosAeronave.getNome());
-        System.out.println(dados.dadosAeronave.getNumeroSerie());
+        try {
+            System.out.println("Aeronave: "+dados.dadosAeronave.getIdAeronave());
+            System.out.println(dados.dadosAeronave.getNome());
+            System.out.println(dados.dadosAeronave.getNumeroSerie());
+            System.out.println();
+            System.out.println("Modelo: "+dados.dadosAeronave.getModelo().getIdModelo());
+            System.out.println(dados.dadosAeronave.getModelo().getNome());
+            System.out.println(dados.dadosAeronave.getModelo().getTipo());
+            System.out.println();
+            System.out.println("Fabricante: "+dados.dadosAeronave.getModelo().getFabricante().getIdFabricante());
+            System.out.println(dados.dadosAeronave.getModelo().getFabricante().getNome());
+            System.out.println(dados.dadosAeronave.getModelo().getFabricante().getDataFundacaoFormatada());
+        } catch (ParseException ex) {
+            Logger.getLogger(TabelaObserver.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
